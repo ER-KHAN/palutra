89153bbdc21a7f9eb7b815ba0fb78012659376c9
ER-KHAN <mmmpolinagin@gmail.com>
2025-02-27 23:03:26 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+function red(){
+    const block = document.getElementById("block")
+    block.style.background = "#af0000"
+}
+function green(){
+    const block = document.getElementById("block")
+    block.style.background = "green"
+}
+function blue(){
+    const block = document.getElementById("block")
+    block.style.background = "blue"
+}
+function yellow(){
+    const block = document.getElementById("block")
+    block.style.background = "yellow"
+}
+function orange(){
+    const block = document.getElementById("block")
+    block.style.background = "orange"
+}
+function blue1(){
+    const block = document.getElementById("block")
+    block.style.background = "rgb(111, 111, 235)"
+}
+function black(){
+    const block = document.getElementById("block")
+    block.style.background = "#1e1e1e"
+}
+function brown(){
+    const block = document.getElementById("block")
+    block.style.background = "brown"
+}
+function white(){
+    const block = document.getElementById("block")
+    block.style.background = "white"
+}
+function purple(){
+    const block = document.getElementById("block")
+    block.style.background = "purple"
+}
+function dark_green(){
+    const block = document.getElementById("block")
+    block.style.background = "rgb(0, 81, 0)"
+}
+function red1(){
+    const block = document.getElementById("block")
+    block.style.background = "rgb(166, 0, 0)"
+}
+function c(){
+    const block = document.getElementById("block")
+    block.style.background = "white"
+}
